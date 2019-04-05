8c1ceb90b585789bc7dcf4adefff59ea20c31f5f
Emperor <daniel.cs20@gmail.com>
2019-04-05 23:30:32 +0200

    Add project base files.

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { join } from "path";
+
+@Module({
+    imports: [
+        GraphQLModule.forRoot({
+            typePaths: ['./**/*.graphql'],
+            definitions: {
+                path: join(process.cwd(), 'src/graphql.ts'),
+            },
+        }),
+    ],
+})
+export class AppModule {}
