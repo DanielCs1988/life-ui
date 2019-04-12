89b26d8b2439d372b8e92ec6ef815b902f1b686e
Emperor <daniel.cs20@gmail.com>
2019-04-13 00:36:12 +0200

    Add DB module: postgres with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,29 @@
-import { Module } from "@nestjs/common";
-import { GraphQLModule } from "@nestjs/graphql";
+import {Module} from '@nestjs/common';
+import {GraphQLModule} from '@nestjs/graphql';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
-import { UsersModule } from './users/users.module';
-import { QuestsModule } from './quests/quests.module';
+import {UsersModule} from './users/users.module';
+import {QuestsModule} from './quests/quests.module';
 
 @Module({
-    imports: [
-        UsersModule,
-        QuestsModule,
-        GraphQLModule.forRoot({
-            autoSchemaFile: 'schema.gql',
-            installSubscriptionHandlers: true,
-        }),
-    ],
+  imports: [
+    UsersModule,
+    QuestsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      installSubscriptionHandlers: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: 'life_ui',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+}
